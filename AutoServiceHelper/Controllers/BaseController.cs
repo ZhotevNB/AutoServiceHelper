4a4a75ad4e8c2913043257c3f2916ec0bab836be
zhotevnb@gmail.com <zhotevnb@gmail.com>
2022-03-10 18:32:21 +0200

    Add BaseController and adjust HomeController

---
+++ AutoServiceHelper/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoServiceHelper.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+        
+    }
+}
