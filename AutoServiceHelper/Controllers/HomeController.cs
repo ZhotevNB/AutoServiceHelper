772e9b875d615deb8366b50bb3d832a5dda1d9f9
zhotevnb@gmail.com <zhotevnb@gmail.com>
2022-03-12 18:22:02 +0200

    New Login page added , add Logout button and functionality, customise the navigation

--- AutoServiceHelper/Controllers/HomeController.cs
+++ AutoServiceHelper/Controllers/HomeController.cs
@@ -28,5 +28,7 @@ namespace AutoServiceHelper.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
+       
     }
 }
