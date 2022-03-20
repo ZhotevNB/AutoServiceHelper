19e73b75d9618f37366c0d5e2f3937415d6d5e6c
zhotevnb@gmail.com <zhotevnb@gmail.com>
2022-03-20 18:16:36 +0200

    Add CarsControler

---
+++ AutoServiceHelper/Controllers/CarsController.cs
@@ -0,0 +1,31 @@
+﻿using AutoServiceHelper.Core.Models.Cars;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoServiceHelper.Controllers
+{
+    public class CarsController : BaseController
+    {
+        public IActionResult AddCar()=> View();
+        
+
+        [HttpPost]
+        public IActionResult AddCar(AddCarFormModel car)
+        {
+            
+            return Redirect("/Cars/MyCars");
+        }
+        public IActionResult MyCars() => View();
+
+
+     
+        public IActionResult AddCarIssue(Guid id) => View();
+
+
+        [HttpPost]
+        public IActionResult AddIssue(string carIssue)
+        {
+            
+            return View();
+        }
+    }
+}
