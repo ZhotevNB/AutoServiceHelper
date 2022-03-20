f795233b93fe9f98636293d7ab6889540bccd2ca
zhotevnb@gmail.com <zhotevnb@gmail.com>
2022-03-20 18:21:05 +0200

    Add AddCarViewModel

---
+++ AutoServiceHelper.Core/Models/Cars/AddCarFormModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AutoServiceHelper.Core.Models.Cars
+{
+    public class AddCarFormModel
+    {
+        public string Manifacture { get; init; }
+              
+        public string Model { get; init; }
+               
+        public int Year { get; init; }
+      
+        public string Vin { get; init; }
+               
+        public string Color { get; init; }
+
+    }
+}
