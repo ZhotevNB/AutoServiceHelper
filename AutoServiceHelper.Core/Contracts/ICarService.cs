1d5e6e161aaa6c0494947b33e4957955c8c14641
zhotevnb@gmail.com <zhotevnb@gmail.com>
2022-03-23 13:10:56 +0200

    adding functionaliti user to add Car and see all of his cars

---
+++ AutoServiceHelper.Core/Contracts/ICarService.cs
@@ -0,0 +1,21 @@
+﻿using AutoServiceHelper.Core.Models.Cars;
+using AutoServiceHelper.Infrastructure.Data.Common;
+using Microsoft.AspNetCore.Identity;
+
+namespace AutoServiceHelper.Core.Contracts
+{
+    public interface ICarService
+    {
+
+        public Task AddCar(AddCarFormModel car,string userId);
+
+        public IEnumerable<CarViewModel> AllCars (string userId);
+
+        public string AddIssue(string carId,string userId);
+
+        public IEnumerable<ViewIssueModel> ViewIssues( string carId);
+
+        public IEnumerable<string> GetIssueTypes();
+
+    }
+}
---
+++ AutoServiceHelper.Core/Models/Cars/AddIssueFormModel.cs
@@ -0,0 +1,23 @@
+﻿using AutoServiceHelper.Infrastructure.Data.Constants;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutoServiceHelper.Core.Models.Cars
+{
+    public class AddIssueFormModel
+    {
+        [Required]
+        public TypeActivity Type { get; set; }
+                          
+        [Required]
+        [Range(DataConstants.OddometerMinValue, DataConstants.OddometerMaxValue)]
+        public int CarOdometer { get; set; }
+          
+        [Required]
+        [StringLength(DataConstants.DiscriptionMinLen)]
+        public string Description { get; set; }
+
+        public IEnumerable<string> ListTypes { get; set; }
+
+    }
+}
