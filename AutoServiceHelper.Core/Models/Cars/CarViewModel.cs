daa787d8d848a4c95163a8c64ee11c5b812b4be3
zhotevnb@gmail.com <zhotevnb@gmail.com>
2022-03-23 17:35:23 +0200

    Adding functionality to cars controller

---
+++ AutoServiceHelper.Core/Models/Cars/CarViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace AutoServiceHelper.Core.Models.Cars
+{
+    public class CarViewModel
+    {
+        
+        public string Id { get; set; }
+             
+        public string Manifacture { get; set; }
+
+        public string UserId { get; set; }
+              
+        public string Model { get; set; }
+               
+        public int Year { get; set; }
+            
+        public string Vin { get; set; }
+
+        public string Color { get; set; }
+    }
+}
--- AutoServiceHelper/Controllers/CarsController.cs
+++ AutoServiceHelper/Controllers/CarsController.cs
@@ -1,31 +1,83 @@
-﻿using AutoServiceHelper.Core.Models.Cars;
+﻿using AutoServiceHelper.Core.Contracts;
+using AutoServiceHelper.Core.Models.Cars;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AutoServiceHelper.Controllers
 {
     public class CarsController : BaseController
     {
-        public IActionResult AddCar()=> View();
-        
+        private readonly ICarService carServices;
+        private readonly UserManager<IdentityUser> userManager;
 
-        [HttpPost]
-        public IActionResult AddCar(AddCarFormModel car)
+        public CarsController(ICarService _carServices, UserManager<IdentityUser> _userManager)
         {
-            
-            return Redirect("/Cars/MyCars");
+            carServices = _carServices;
+            userManager = _userManager;
         }
-        public IActionResult MyCars() => View();
 
-
-     
-        public IActionResult AddCarIssue(Guid id) => View();
+        public IActionResult AddCar() => View();
 
 
         [HttpPost]
-        public IActionResult AddIssue(string carIssue)
+        public async Task<IActionResult> AddCar(AddCarFormModel car)
         {
-            
-            return View();
+            var user = await userManager.GetUserAsync(User);
+            var currentUserId = await userManager.GetUserIdAsync(user);
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            carServices.AddCar(car, currentUserId);
+
+            return RedirectToAction("MyCars");
+
+
+
+        }
+        public async Task<IActionResult> MyCars()
+        {
+            var user = await userManager.GetUserAsync(User);
+            var currentUserId = await userManager.GetUserIdAsync(user);
+
+            var model = carServices.AllCars(currentUserId);
+
+            return View(model);
+        }
+
+
+        public IActionResult Issues(string carId)
+        {
+            var model = carServices.ViewIssues(carId);
+
+            return View(model);
+        }
+
+
+        public IActionResult AddIssue(string id)
+        {
+            var model = carServices.GetIssueTypes();
+            var Issue = new AddIssueFormModel()
+            {
+                ListTypes = model
+            };
+            return View(Issue);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddIssue(AddIssueFormModel carIssue,string carId)
+        {
+            var user = await userManager.GetUserAsync(User);
+            var userId = await userManager.GetUserIdAsync(user);
+
+            carServices.AddIssue(carIssue,carId, userId);
+            return RedirectToAction("MyCars");
+        }
+        public IActionResult FixIssue(string issueId)
+        {
+            carServices.FixIssue(issueId);
+            return RedirectToAction("MyCars");
         }
     }
 }
