b7eb0585e006e8dfcf1fea4b004f24a099cad794
zhotevnb@gmail.com <zhotevnb@gmail.com>
2022-03-23 17:36:49 +0200

    trying to add settings functionality

---
+++ AutoServiceHelper/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoServiceHelper.Controllers
+{
+    public class UsersController : BaseController
+    {
+
+       
+        public IActionResult Settings()=> View();
+        
+    }
+}
