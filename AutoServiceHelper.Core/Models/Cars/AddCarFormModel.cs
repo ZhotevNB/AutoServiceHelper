82ac17614a656feb7e9df914a5f3034c3ccb5a33
zhotevnb@gmail.com <zhotevnb@gmail.com>
2022-03-29 09:06:55 +0300

    Add constraints to AddCarViewModel

--- AutoServiceHelper.Core/Models/Cars/AddCarFormModel.cs
+++ AutoServiceHelper.Core/Models/Cars/AddCarFormModel.cs
@@ -1,15 +1,21 @@
-﻿namespace AutoServiceHelper.Core.Models.Cars
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoServiceHelper.Core.Models.Cars
 {
     public class AddCarFormModel
     {
+        [Required]
         public string Manifacture { get; init; }
-              
+
+        [Required]
         public string Model { get; init; }
-               
+
+        [Range(1990, 2022)]
         public int Year { get; init; }
-      
+        [RegularExpression(@"/\b[(A-H|J-N|P|R-Z|0-9)]{17}\b/", ErrorMessage = "Invalid VIN")]
         public string Vin { get; init; }
-               
+        [Required]
+        [StringLength(30,MinimumLength =3)]
         public string Color { get; init; }
 
     }
