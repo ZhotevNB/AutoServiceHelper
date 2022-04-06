600a2f523e453c91bcde70a87eef1d38ff6b0198
zhotevnb@gmail.com <zhotevnb@gmail.com>
2022-04-06 15:57:38 +0300

    Administrator role manage functionality

---
+++ AutoServiceHelper.Core/Models/Users/UserChangeRollViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoServiceHelper.Core.Models.Users
+{
+    public class UserChangeRollViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool WantToBeManager { get; set; }
+
+        public bool WantToBeMechanic { get; set; }
+
+
+
+    }
+}
--- AutoServiceHelper.Core/Services/UserService.cs
+++ AutoServiceHelper.Core/Services/UserService.cs
@@ -3,6 +3,7 @@ using AutoServiceHelper.Core.Models.Users;
 using AutoServiceHelper.Infrastructure.Data.Common;
 using AutoServiceHelper.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoServiceHelper.Core.Services
 {
@@ -23,81 +24,86 @@ namespace AutoServiceHelper.Core.Services
 
         public Task ChangeUserInfo(string userId, UsersSetingsFormModel model)
         {
-            var info = repository.All<UserInfo>().Where(u => u.UserId == userId).FirstOrDefault();
+            var info = repository.All<UserInfo>()
+                .Where(u => u.UserId == userId)
+                .FirstOrDefault();
+
             if (info == null)
             {
-                info = new UserInfo
-                {
-                    UserId = userId,
-                    NickName = model.NickName,
-                    FirstName = model.FirstName,
-                    LastName = model.LastName
-
-                };
+                info = new UserInfo();
+                info.UserId = userId;
                 repository.Add(info);
             }
-
             info.NickName = model.NickName;
             info.FirstName = model.FirstName;
             info.LastName = model.LastName;
             info.AskToChangeRollManager = model.Manager;
             info.AskToChangeRollMechanic = model.Mechanic;
 
-          repository.SaveChanges();
+           
+            repository.SaveChanges();
 
             return Task.CompletedTask;
         }
 
-        public Task<IEnumerable<UserChangeRollViewModel>> GetUserForMechanicRolleChange()
+
+        public async Task<IEnumerable<UserChangeRollViewModel>> GetUsers()
         {
-            var users = repository.All<UserInfo>().Where(u => u.AskToChangeRollMechanic == true).Select(u => new UserChangeRollViewModel
-            {
-                Id = u.UserId
-            });
-
-            return GetUsersName(users);
-        }
-        public Task<IEnumerable<UserChangeRollViewModel>> GetUserForManagerRolleChange()
-        {
-            var users = repository.All<UserInfo>().Where(u => u.AskToChangeRollManager == true).Select(u => new UserChangeRollViewModel
-            {
-                Id = u.UserId
-            });
-
-            return GetUsersName(users);
-
-        }
-        private async Task<IEnumerable<UserChangeRollViewModel>> GetUsersName(IEnumerable<UserChangeRollViewModel> users)
-        {
-            var usersName = repository.All<IdentityUser>().Select(x => new
-            {
-                Id = x.Id,
-                Name = x.UserName
-            }).ToList();
-
-            foreach (var user in users)
-            {
-                user.Name = usersName.First(u => u.Id == user.Id).Name;
-            }
+            var users = await repository
+                .All<UserInfo>()
+                .Select(u => new UserChangeRollViewModel
+                {
+                    Id = u.UserId,
+                    Name = $"{u.FirstName} {u.LastName}",
+                    WantToBeManager = u.AskToChangeRollManager,
+                    WantToBeMechanic = u.AskToChangeRollMechanic
+                }).ToListAsync();
 
             return users;
+
         }
 
-        public UsersSetingsFormModel GetUserInfo(string userId)
+        public async Task<UserChangeRollViewModel> GetUserInfoById()
         {
-         var respons=repository.All<UserInfo>()
-                .Where(x => x.UserId == userId)
-                .Select(x=>new UsersSetingsFormModel
-           {
-                    NickName=x.NickName,
-                    FirstName=x.FirstName,
-                    LastName=x.LastName,
-                    UserId=userId,
-                    Manager=x.AskToChangeRollManager,
-                    Mechanic=x.AskToChangeRollMechanic
+            var users = await repository
+                .All<UserInfo>()
+                .Select(u => new UserChangeRollViewModel
+                {
+                    Id = u.UserId,
+                    Name = $"{u.FirstName} {u.LastName}",
+                    WantToBeManager = u.AskToChangeRollManager,
+                    WantToBeMechanic = u.AskToChangeRollMechanic
+                }).FirstAsync();
 
-           })
-                .FirstOrDefault();
+            return users;
+
+        }
+
+
+        public async Task<UsersSetingsFormModel> GetUserInfo(string userId)
+        {
+            var respons = await repository.All<UserInfo>()
+                   .Where(x => x.UserId == userId)
+                   .Select(x => new UsersSetingsFormModel
+                   {
+                       NickName = x.NickName,
+                       FirstName = x.FirstName,
+                       LastName = x.LastName,
+                       UserId = userId,
+                       Manager = x.AskToChangeRollManager,
+                       Mechanic = x.AskToChangeRollMechanic
+
+                   })
+                   .FirstOrDefaultAsync();
+
+            return respons;
+        }
+        public async Task<IdentityUser> GetUserById(string userId)
+        {
+
+            var respons = await repository.All<IdentityUser>()
+                   .Where(x => x.Id == userId)
+                   .FirstOrDefaultAsync();
 
             return respons;
         }
